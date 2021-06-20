e4998720ba9bd9d4ad2f4d8843ad10c5f2c5329b
Joao Victor Amorim Vieira <amorimvictorjoao3@gmail.com>
2021-06-20 13:08:20 -0300

    adding new pages and entry to gitignore

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,74 +1,101 @@
-import React, { useState } from "react";
+import { Entypo, Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
 import {
-  SafeAreaView,
-  Text,
-  Image,
-  TouchableOpacity,
-  StyleSheet,
-  Dimensions,
-} from "react-native";
+    SafeAreaView,
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions,
+    View,
+} from 'react-native';
 
-import { Button } from "../components/Button";
-
-import wateringImg from "../assets/watering.png";
-import colors from "../styles/colors";
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false);
+    const navigation = useNavigation();
+    function handleStart() {
+        navigation.navigate('UserIdentification');
+    }
 
-  function handleVisibility() {
-    setVisible(true);
-  }
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie{'\n'} suas plantas de{'\n'} forma fácil
+                </Text>
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie{"\n"} suas plantas{"\n"} de forma fácil
-      </Text>
-      <Image source={wateringImg} style={styles.image}></Image>
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-      <Button title=">" />
-    </SafeAreaView>
-  );
+                <Image
+                    source={wateringImg}
+                    style={styles.image}
+                    resizeMode="contain"
+                ></Image>
+
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas {'\n'}plantas. Nós cuidamos
+                    de lembrar você
+                    {'\n'}
+                    sempre que precisar.
+                </Text>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.6}
+                    onPress={handleStart}
+                >
+                    <Text>
+                        <Feather
+                            name="chevron-right"
+                            style={styles.buttonIcon}
+                        />
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  title: {
-    marginTop: 38,
-    fontSize: 32,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: colors.heading,
-  },
-  subtitle: {
-    textAlign: "center",
-    fontSize: 18,
-    paddingHorizontal: 20,
-    color: colors.heading,
-  },
-  button: {
-    backgroundColor: colors.green,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 16,
-    marginBottom: 40,
-    height: 56,
-    width: 56,
-  },
-  image: {
-    width: 292,
-    height: 284,
-  },
-  buttonText: {
-    color: colors.white,
-    fontSize: 24,
-  },
+    container: {
+        flex: 1,
+    },
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontFamily: fonts.heading,
+        marginTop: 38,
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        lineHeight: 34,
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        color: colors.heading,
+    },
+    image: {
+        height: Dimensions.get('window').width * 0.7,
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 30,
+        height: 56,
+        width: 56,
+    },
+    buttonIcon: {
+        color: colors.white,
+        fontSize: 32,
+    },
 });
